5b2864422a8122833a8677b750a618cb711d7d2b
Hyojin Kwon <raccoonhj33@gmail.com>
2020-03-15 20:01:57 +0900

    200315 Snippet test - test_snippet_delete (tests.py)

--- app/snippets/tests.py
+++ app/snippets/tests.py
@@ -1,3 +1,5 @@
+import random
+
 from django.test import TestCase
 
 # Create your tests here.
@@ -50,13 +52,33 @@ class SnippetTest(APITestCase):
         }
 
         response = self.client.post(url, data=data)
-        print('response.data >> ', response.data)
+        # print('response.data >> ', response.data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         pk = response.data['pk']
         snippet = Snippet.objects.get(pk=pk)
-        print('pk >> ', pk)
-        print('snippet, snippet.pk >> ', snippet, snippet.pk)
-        print('SnippetSerializer(snippet).data >> ', SnippetSerializer(snippet).data)
+        # print('pk >> ', pk)
+        # print('snippet, snippet.pk >> ', snippet, snippet.pk)
+        # print('SnippetSerializer(snippet).data >> ', SnippetSerializer(snippet).data)
         self.assertEqual(SnippetSerializer(snippet).data, response.data)
         self.assertEqual(Snippet.objects.count(), 1)
+
+    def test_snippet_delete(self):
+        # 객체 5개 생성
+        snippets = [Snippet.objects.create(code='3') for i in range(5)]
+        print('snippets5 >> ', snippets)
+        # 객체가 5개인지 확인
+        self.assertEqual(Snippet.objects.count(), 5)
+
+        # 아무거나 1개 선택
+        snippet = random.choice(snippets)
+        print('snippet, snippet.pk >> ', snippet, snippet.pk)
+
+        url = f'/snippets/{snippet.pk}/'
+        # delete API 실행
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        # 객체가 4개가 되었는지 확인
+        self.assertEqual(Snippet.objects.count(), 4)
+        print('Snippet.objects.all() >> ', Snippet.objects.all())
+        self.assertFalse(Snippet.objects.filter(pk=snippet.pk).exists())
