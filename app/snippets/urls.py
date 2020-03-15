0eea77acdf129c581858549c1b66dc46ac8d6328
Hyojin Kwon <raccoonhj33@gmail.com>
2020-03-15 17:20:40 +0900

    200315 snippet list, detail based on class-based view(APIView) in apis.py

--- app/snippets/urls.py
+++ app/snippets/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from snippets import views
+from snippets import views, apis
 
 app_name = 'snippets'
 urlpatterns = [
-    path('', views.snippet_list),
-    path('<int:pk>/', views.snippet_detail),
+    # views.py
+    # path('', views.snippet_list),
+    # path('<int:pk>/', views.snippet_detail),
+
+    # apis.py (class-based view)
+    path('', apis.SnippetListCreateAPIViwe.as_view()),
+    path('<int:pk>/', apis.SnippetRetrieveUpdateDestroyAPIView.as_view()),
 ]
