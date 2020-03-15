0565661c8059e8e051a7c9db3f665fb19228bb0f
Hyojin Kwon <raccoonhj33@gmail.com>
2020-03-15 16:10:50 +0900

    200315 used ModelSerializer to make SnippetSerializer

--- app/snippets/serializers.py
+++ app/snippets/serializers.py
@@ -3,22 +3,16 @@ from rest_framework import serializers
 from snippets.models import LANGUAGE_CHOICES, STYLE_CHOICES, Snippet
 
 
-class SnippetSerializer(serializers.Serializer):
-    pk = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    code = serializers.CharField()
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
-
-    # create(data)
-    def create(self, validated_data):
-        return Snippet.objects.create(**validated_data)
-
-    # update(instance, data)
-    def update(self, instance, validated_data):
-        for key, value in validated_data.items():
-            # instance.key = value
-            setattr(instance, key, value)
-        instance.save()
-        return instance
+# ModelSerializer - create(), update() 메소드 기본 구현
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = (
+            'pk',
+            'title',
+            'code',
+            'linenos',
+            'language',
+            'style',
+            'created',
+        )
