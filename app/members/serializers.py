ec0dfbf52f63be6b54bfa39da967b77ff8f966c0
Hyojin Kwon <raccoonhj33@gmail.com>
2020-03-20 00:30:15 +0900

    200319 retrun User information form AuthTokenAPIView again

--- app/members/serializers.py
+++ app/members/serializers.py
@@ -1,8 +1,10 @@
 # serializer에서 필드를 동적으로 수정
 # 응답으로 제한된 수의 매개변수를 추출하기 위해 웹 API가 훨씬 쉬워짐
 # 초기화 시점에 serializer가 사용해야 할 필드를 설정한다고 가정
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from members.models import User
+
+User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
